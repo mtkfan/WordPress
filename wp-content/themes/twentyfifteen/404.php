819ff2cab65d023587cbace8f1a34d93587da761
Mr. Feng <153568795@qq.com>
2015-01-25 09:29:44 +0800

    Update 404.php

--- wp-content/themes/twentyfifteen/404.php
+++ wp-content/themes/twentyfifteen/404.php
@@ -18,8 +18,21 @@ get_header(); ?>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentyfifteen' ); ?></p>
-
+					<p><?php //_e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentyfifteen' ); ?></p>
+					<p>
+					<script type="text/javascript"><!--
+						google_ad_client = "ca-pub-4968802918299735";
+						/* square250 */
+						google_ad_slot = "3796389445";
+						google_ad_width = 250;
+						google_ad_height = 250;
+						//-->
+					</script>
+					<script type="text/javascript"
+						src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+					</script>
+					</p>
+					
 					<?php get_search_form(); ?>
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
